acb35d7c7f375facfcafa93aeb5fde1595549249
GustavoZetta <gustavo.zzetta@gmail.com>
2024-07-31 20:41:42 -0300

    a ton of things

---
+++ src/main/java/com/zetagroup/combatreforged/Commands/Class.java
@@ -0,0 +1,25 @@
+package com.zetagroup.combatreforged.Commands;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class Class implements CommandExecutor {
+    static String cmd = "class"; // Defining the "/class" command as a "static" for less RAM usage
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String string, String[] arguments) {
+        if (!(command.getName().equals(cmd))) { return true; } // If the command name executed is not "/class", end the function by returning "true" for "Executed Right"
+        if (!(sender instanceof Player)) { return true; } // If the sender of the command is not a player, end the function by returning "true" for "Executed Right"
+        // If any function return false then it will show the default paper error message
+        // If it returns true, the message will not appear
+        Player player = (Player) sender; // Defining the sender as a player, since now we know its really a player
+
+
+        Location loc = player.getLocation();
+
+        return true;
+    }
+}
--- src/main/java/com/zetagroup/combatreforged/ConfigHandler/FileManager.java
+++ src/main/java/com/zetagroup/combatreforged/ConfigHandler/FileManager.java
@@ -7,57 +7,70 @@ import java.io.File;
 import java.util.logging.Level;
 
 public class FileManager {
-    private String getLog(int LogNum) {
-        File file = null;
-        YamlConfiguration config = null;
-        String prefix = null;
+    private String getLog(int LogNum) { // private function that returns the log messages
+        File file = null; // Defining "file"
+        YamlConfiguration config = null; // Defining "config"
+        String prefix = null; // Defining "prefix" (for messages)
 
-        try {
-            file = new File(Bukkit.getUpdateFolderFile().getPath(), "mensagens/log.yml");
-            config = YamlConfiguration.loadConfiguration(file);
-            prefix = config.getString("prefix");
-        } catch (NullPointerException Erro) {
-            Bukkit.getLogger().log(Level.WARNING, "Erro carregando as mensagens do log, usando mensagens padrão do plugin (Delete o arquivo para reseta-lo)");
-            prefix = "§c[MultiEconomy]§f";
+        try { // A try executes a function and if any error happens, you can catch that error so the class doesn't crash
+            file = new File(Bukkit.getUpdateFolderFile().getPath(), "messages/msglog.yml"); // Defining file as a actual file
+            config = YamlConfiguration.loadConfiguration(file); // Defining config as a actual config variable
+            prefix = config.getString("prefix").replace('&', '§'); // Defining the prefix as the actual log prefix
+        } catch (NullPointerException Error) { // Catches the error and send a message to ghe log
+            Bukkit.getLogger().log(Level.WARNING, "Error loading log messages, using default stored plugin messages (Delete the file to reset it)");
+            prefix = "§8[§cCombatReforged§8]§7"; // Defines prefix as a backup prefix
         }
 
-        switch (LogNum) {
-            case 404: {
-                try {
-                    String msg = config.getString("arquivo-inexistente").replace('&', '§');
-                    msg.replace("{prefix}", prefix);
-                    return msg;
-                } catch (NullPointerException Erro) {
-                    return prefix + "Erro 404: Arquivo não encontrado";
+        switch (LogNum) { // Switch is basically a if but more optimized
+            case 404: { // if (LogNum == 404) {...}
+                try { // Try to catch any error
+                    String msg = config.getString("No File").replace('&', '§'); // replaces the & char with the actual color character
+                    msg.replace("{prefix}", prefix); // Transform the "{prefix}" placeholder into the actual prefix
+                    return msg; // return the message the plugin got from the config
+                } catch (NullPointerException Error) { // Catch any error
+                    Bukkit.getLogger().log(Level.WARNING, prefix + " Error while trying to get log messages from the messages/msglog.yml file"); // Sends a warning about the error from the log messages
+                    return prefix + " Error 404: File not Found"; // return the backup message
                 }
             }
-            case 405: {
+            case 405: { // else if (LogNum == 405) {...}
                 try {
-                    String msg = config.getString("configuracao-inexistente").replace('&', '§');
-                    msg.replace("{prefix}", prefix);
-                    return msg;
-                } catch (NullPointerException Erro) {
-                    return prefix + "Erro 405: Path YAML não encontrado";
+                    String msg = config.getString("No Config").replace('&', '§'); // replaces the & char with the actual color character
+                    msg.replace("{prefix}", prefix); // Transform the "{prefix}" placeholder into the actual prefix
+                    return msg; // return the message the plugin got from the config
+                } catch (NullPointerException Error) { // Catch any error
+                    Bukkit.getLogger().log(Level.WARNING, prefix + " Error while trying to get log messages from the messages/msglog.yml file"); // Sends a warning about the error from the log messages
+                    return prefix + " Error 405: Path YAML not found"; // return the backup message
                 }
             }
-            default: {
-                return prefix + "Erro desconhecido, por favor, contate o desenvolvedor do plugin";
+            case 406: { // else if (LogNum == 406) {...}
+                try {
+                    String msg = config.getString("Unknow File Type").replace('&', '§'); // replaces the & char with the actual color character
+                    msg.replace("{prefix}", prefix); // Transform the "{prefix}" placeholder into the actual prefix
+                    return msg; // return the message the plugin got from the config
+                } catch (NullPointerException Error) { // Catch any error
+                    Bukkit.getLogger().log(Level.WARNING, prefix + " Error while trying to get log messages from the messages/msglog.yml file"); // Sends a warning about the error from the log messages
+                    return prefix + " Error 406: File is not a YAML Config file"; // return the backup message
+                }
+            }
+            default: { //else {...} -> Its an default case if other comparations were not found
+                return prefix + " Unknow error, please contact the developer"; // return the backup message
             }
         }
     }
 
-    public void getFile(String filepath) {
-        if (filepath == null || filepath.trim().isEmpty()) { return; }
+    public void getFile(String filepath) { // Function for getting the file for Yaml(.yml) Configs
+        if (filepath == null || filepath.trim().isEmpty()) { return; } // If the filepath is empty, finishes the function
 
-        if (filepath.endsWith(".yml")) {
-            File file = new File(Bukkit.getUpdateFolderFile().getPath(), filepath);
+        if (filepath.endsWith(".yml")) { // If the filetype is a .yml, continues
+            File file = new File(Bukkit.getUpdateFolderFile().getPath(), filepath); // Gets the file with the main file path
 
-            if (!file.exists()) {
+            if (!file.exists()) { // If file does not exists alredly, sends an error and ends the function
                 Bukkit.getLogger().log(Level.WARNING, getLog(404));
                 return;
             }
         } else {
-
+            Bukkit.getLogger().log(Level.WARNING, getLog(406));
+            return;
         }
     }
 }
