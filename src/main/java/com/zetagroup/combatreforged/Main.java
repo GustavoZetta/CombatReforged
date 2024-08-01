3aef12f331adc1dd6f7d20833e34506a9946afef
GustavoZetta <gustavo.zzetta@gmail.com>
2024-08-01 13:22:12 -0300

    Update Main.java

--- src/main/java/com/zetagroup/combatreforged/Main.java
+++ src/main/java/com/zetagroup/combatreforged/Main.java
@@ -1,5 +1,6 @@
 package com.zetagroup.combatreforged;
 
+import com.zetagroup.combatreforged.Commands.Class
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,7 +9,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         ConfigManager ConfigInstance = new ConfigManager(); // Initiating the config instance (only one is used for the entire plugin)
-
+        Class CommandClassInstance = new Class(ConfigInstance);
     }
 
     @Override
