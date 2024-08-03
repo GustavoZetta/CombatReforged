2e204f1767228c94ba78c12d205b1b621cc25441
Pigzzrule <dragonslayer10312@gmail.com>
2024-08-03 19:10:58 -0400

    changes!~
    grrrr lets try this out~!

--- src/main/java/com/zetagroup/combatreforged/Commands/Combo.java
+++ src/main/java/com/zetagroup/combatreforged/Commands/Combo.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -26,9 +27,11 @@ public class Combo implements CommandExecutor {
         // If any function return false then it will show the default paper error message
         // If it returns true, the message will not appear
         Player player = (Player) sender; // Defining the sender as a player, since now we know its really a player
-        player.setVelocity(player.;
+        Vector unitVector = new Vector(player.getLocation().getDirection().getX(), player.getLocation().getDirection().getY() , player.getLocation().getDirection().getZ());
+        unitVector = unitVector.normalize();
+        player.setVelocity(unitVector.multiply(2));
         return true;
+    
 
-        player.
     }
 }
